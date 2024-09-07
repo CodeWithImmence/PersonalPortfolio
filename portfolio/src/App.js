4494c5cb5f4053004285598226a86804731d2b16
Code With Immence <rajaivishal41@gmail.com>
2024-09-08 02:46:40 +0530

    Updated correct homepage

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcoe to the Code with Immence </h1>
+      <h1>Welcome to the Code with Immence </h1>
     </div>
   );
 }
